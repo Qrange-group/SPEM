4f37114bc0fe9778017ec06782f312a6962f37cb
zhongshsh <zhongshsh5@mail2.sysu.edu.cn>
2023-05-09 16:42:38 +0800

    update

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,5 @@
-# A simple torch style logger
-# (C) Wei YANG 2017
 from __future__ import absolute_import
 import matplotlib.pyplot as plt
-import os
-import sys
 import numpy as np
 
 __all__ = ["Logger", "LoggerMonitor", "savefig"]
